c4bda628e814c3baf6e4ab157d5ea23536b8de2f
Osman Fırat <osmanfrat@yahoo.com>
2023-05-27 23:27:45 +0300

    boss spawn fixed

--- code/Assets/Scripts/PlayerController.cs
+++ code/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb;
+    public Rigidbody playerRb;
     private float speed = 5.0f;
     public bool hasPowerUp = false;
+    public bool gunPowerUp = false;
     private float powerUpStrength = 15.0f;
     public GameObject powerUpIndicator;
+    public GameObject bullet;
     private float rotatingPowerUp;
+
+    private float fireElapsedTime = 0;
+    public float fireDelay = 0.3f;
+    private float jumpPower = 15.0f;
+    private float smashPower = 50.0f;
+    private float smashRange = 12.0f;
+    private bool smashEnabled;
 
     void Start()
     {
@@ -39,8 +48,67 @@ public class PlayerController : MonoBehaviour
             powerUpIndicator.transform.Rotate(new Vector3(0, 3, 0));
         }
         
+        fireElapsedTime += Time.deltaTime;
+        if(Input.GetKey(KeyCode.Mouse0) && gunPowerUp == true && fireElapsedTime >= fireDelay)
+        {
+            fireElapsedTime = 0;
+            shoot();
+        }
+
+        if(Input.GetKey(KeyCode.Space) && smashEnabled  && fireElapsedTime >= fireDelay)
+        {
+            fireElapsedTime = 0;
+            StartCoroutine(jump());
+            smash();
+        }
         
 
+    }
+
+    IEnumerator jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.3f);
+        playerRb.AddForce(Vector3.down * jumpPower *2, ForceMode.Impulse);
+
+    }
+    private void smash()
+    {
+        Enemy[] enemyList = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy enemy in enemyList)
+        {
+
+            Vector3 awayFromPlayer = enemy.gameObject.transform.position - transform.position;
+
+            if (awayFromPlayer.magnitude <= smashRange)
+            {
+
+                float power = ( smashPower - ( (awayFromPlayer.magnitude / smashRange) * smashPower ) );
+                
+                Rigidbody enemyRb = enemy.gameObject.GetComponent<Rigidbody>();
+
+                enemyRb.AddForce(awayFromPlayer.normalized * power, ForceMode.Impulse);
+
+            }
+        }
+    }
+    private void shoot()
+    {
+        //Get a list of all enemies
+        Enemy[] enemyList = FindObjectsOfType<Enemy>();
+        
+        //Create a different bullet for each enemy
+        foreach( Enemy enemy in enemyList)
+        {
+            Vector3 lookToEnemy = enemy.transform.position - transform.position;
+            Vector3 starPos = transform.position + lookToEnemy.normalized + new Vector3(0, 1, 0);
+            
+            Quaternion rotate = Quaternion.LookRotation(lookToEnemy, Vector3.up) * Quaternion.Euler(90, 0, 0);
+
+            // Create bullet and send enemy gameObject in bullet as a target
+            Instantiate(bullet, starPos, rotate).GetComponent<Bullet>().target = enemy.gameObject;
+        }  
     }
 
     // Gaining power up
@@ -54,7 +122,22 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             StartCoroutine(PowerUpCountDownRoutine());
         }
-
+        
+    
+        if(other.CompareTag("GunPowerUP"))
+        {
+            gunPowerUp = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
+        if(other.CompareTag("SmashPowerUp"))
+        {
+            smashEnabled = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
     }
 
     // Power up countdown
@@ -62,11 +145,14 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerUp = false;
+        gunPowerUp = false;
+        smashEnabled = false;
         powerUpIndicator.gameObject.SetActive(false);
     }
 
     // Power up
-    private void OnCollisionEnter(Collision collision) {
+    private void OnCollisionEnter(Collision collision) 
+    {
         if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
         {
             Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
@@ -75,7 +161,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Collided with " + collision.gameObject.name + " with power up set to " + hasPowerUp);
             enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
         }
-        
+
     }
+        
+    
 }
 
--- code/Assets/Scripts/SpawnManager.cs
+++ code/Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,54 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject strongEnemy;
     public GameObject powerUpPrefab;
+    public GameObject gunPowerUp;
+    public GameObject smashPowerUp;
+    public GameObject boss;
     private float spawnRange = 9.0f;
     public int enemyCount;
     public int waveNumber = 1;
     public int powerUpNumber = 1;
     void Start()
     {
+        float repeatRate = Random.Range(5f,10f);
+        
         SpawnEnemyWave(waveNumber);
         SpawnPowerUp(powerUpNumber);
-
+        InvokeRepeating("SpawnStrongEnemy", 5f, 15f);
+        InvokeRepeating("SpawnGunPowerUp", 3f, repeatRate);
+        InvokeRepeating("SpawnSmashPowerUp", 5f, repeatRate);
+        Invoke("SpawnBoss", 25f);
+        
     }
+
 
     void SpawnEnemyWave(int enemyToSpawn)
     {
+        
         for(int i = 0; i < enemyToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
+    }
+
+    void SpawnStrongEnemy()
+    {
+        Instantiate(strongEnemy, GenerateSpawnPosition(), strongEnemy.transform.rotation);
+    }
+
+    void SpawnGunPowerUp()
+    {
+
+        Instantiate(gunPowerUp, GenerateSpawnPosition(), gunPowerUp.transform.rotation);
+        
+    }
+
+    void SpawnSmashPowerUp()
+    {
+        Instantiate(smashPowerUp, GenerateSpawnPosition(), smashPowerUp.transform.rotation);
+        Destroy(smashPowerUp, 3f);  
     }
 
     void SpawnPowerUp(int powerUpToSpawn)
@@ -30,9 +60,18 @@ public class SpawnManager : MonoBehaviour
         for(int i = 0; i < powerUpToSpawn; i++)
         {
             Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+            Destroy(powerUpPrefab, 3f);  
+
         }
     }
     
+    void SpawnBoss()
+    {
+
+        Instantiate(boss, GenerateSpawnPosition(), boss.transform.rotation);
+        
+    }
+
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
@@ -42,8 +81,10 @@ public class SpawnManager : MonoBehaviour
             waveNumber++;
             SpawnEnemyWave(waveNumber);
             SpawnPowerUp(powerUpNumber);
-                
+            SpawnGunPowerUp();
+            SpawnSmashPowerUp();
         }
+
         
     }
 
@@ -56,4 +97,16 @@ public class SpawnManager : MonoBehaviour
 
         return randomPos;
     }
+
+    /*
+    private Vector3 GenerateSpawnPositionForGunPowerUp()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+    */
 }
